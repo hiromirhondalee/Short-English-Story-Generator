a4cbca557a4d5ad6158360af6bfa6d61e375e2d0
hiromirhondalee <106946484+hiromirhondalee@users.noreply.github.com>
2024-12-05 13:35:23 +0900

    Improved User Experience and removed Console Logs

--- src/index.js
+++ src/index.js
@@ -18,12 +18,19 @@ function generateStory (event) {
   let context = "You are a humorous creative story teller and love to create short stories that are entertaining and funny for kids. Your mission is to generate a 150 word story, format the story using basic HTML.Make sure to follow the user instructions. Do not include a title. Format the story into readable paragraphs. Sign the story with 'SheCodes AI' inside a <strong> element at the end of the story. "
   let apiUrl =
     `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
+  let storyContainer = document.querySelector("#story-container");
+  storyContainer.classList.remove("hidden");
+
+    let storyElement = document.querySelector("#story");
+    storyElement.innerHTML = (
+     `<div class="generating">
+        ⏳ Generating a story about ${userInput.value}
+      </div>`
+    );
 
     axios.get(apiUrl).then(displayStory);
   
-    console.log ("Generating Story")
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Context: ${context}`)
 
 }
 
